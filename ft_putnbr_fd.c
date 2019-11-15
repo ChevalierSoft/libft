4a8d87920c1111cf15ab3a08d2bde7df5075b034
ChevalierSoft <ibanarashi@outlook.fr>
2019-11-15 13:29:10 +0100

    adding putnbr

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,55 @@
+
+#include "libft.h"
+
+static int  ft_nb_digit_custo(int n)
+{
+	int nbc;
+
+	if (!n)
+		return (1);
+	nbc = 0;
+	while (n)
+	{
+		n /= 10;
+		nbc++;
+	}
+	return (nbc);
+}
+
+void ft_putnbr_fd(int n, int fd)
+{
+	char			a[12];
+	unsigned char	i;
+	int				nbc;
+
+	if (n == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	nbc = ft_nb_digit_custo(n);
+	i = 0;
+	if (n < 0)
+	{
+		n = -n;
+		a[0] = '-';
+		write(fd, "-", 1);
+	}
+	a[nbc] = 0;
+	while (nbc + 2)
+	{
+		a[--nbc] = n % 10 + '0';
+		n /= 10;
+	}
+	write(fd, a, ft_strlen(a));
+}
+
+/*
+int		main(void)
+{
+	ft_putnbr_fd(-2147483648, 1);
+
+	return (0);
+}
+*/
+
