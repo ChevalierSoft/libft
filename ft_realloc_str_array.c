2c47362c371497c8537fa4c0fadf8e3d8fccfdc7
ChevalierSoft <ibanarashi@outlook.fr>
2021-01-09 03:23:11 +0100

    patching ft_realloc_str_array() to handle smaller arrays in output

--- ft_realloc_str_array.c
+++ ft_realloc_str_array.c
@@ -6,27 +6,31 @@
 /*   By: dait-atm <dait-atm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/16 11:51:03 by dait-atm          #+#    #+#             */
-/*   Updated: 2021/01/06 14:04:33 by dait-atm         ###   ########.fr       */
+/*   Updated: 2021/01/09 03:21:02 by dait-atm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_realloc_str_array(char ***ar, int nb)
+char	**ft_realloc_str_array(char ***ar, unsigned int nb)
 {
 	char	**as;
 	int		i;
 
-	as = malloc(sizeof(char *) * nb);
+	as = malloc(sizeof(char *) * (nb + 1));
 	if (!as)
 		return (NULL);
 	i = 0;
-	while (i < nb - 1 && (*ar)[i])
+	while (i < nb && (*ar)[i])
 	{
 		as[i] = (*ar)[i];
 		i++;
 	}
 	as[i] = NULL;
+	while ((*ar)[i])
+	{
+		free((*ar)[i++]);
+	}
 	free(*ar);
 	return (as);
 }
