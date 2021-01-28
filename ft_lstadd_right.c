ce7108316e5dc59c83da58e78e6478a7dc4b0305
ChevalierSoft <ibanarashi@outlook.fr>
2021-01-28 12:24:32 +0100

    fixed bad declaration

--- ft_lstadd_right.c
+++ ft_lstadd_right.c
@@ -6,13 +6,13 @@
 /*   By: dait-atm <dait-atm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/28 10:11:26 by dait-atm          #+#    #+#             */
-/*   Updated: 2021/01/28 10:34:37 by dait-atm         ###   ########.fr       */
+/*   Updated: 2021/01/28 12:18:31 by dait-atm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lst_add_right(t_list *left, t_list *new_right)
+void	ft_lstadd_right(t_list *left, t_list *new_right)
 {
 	if (!left || !new_right)
 		return ;
